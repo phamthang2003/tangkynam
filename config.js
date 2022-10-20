c181ec209b28028af1dfcb327e9f3a164fd5fb8f
phamthang2003 <116180596+phamthang2003@users.noreply.github.com>
2022-10-20 20:40:56 +0700

    Add files via upload
    tangkynam

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'bạn Kỳ Nam';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng bạn Kỳ Nammmm';
+const contentCard = 'chúc bạn Kỳ Nam có 1 ngày 20/10 vui vẻ, chúc bạn luôn xinh đẹp, luôn giữ sức khỏe và thành công trong mọi việc nhé';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anhkynam.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
